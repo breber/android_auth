de62d1de7a0168e5bf39c20dda140b3f8fb05ed1
Brian Reber <reber.brian@gmail.com>
2012-12-18 20:59:24 -0600

    Add a base authenticated http request task

---
+++ src/com/brianreber/library/AuthenticatedHttpRequest.java
@@ -0,0 +1,89 @@
+package com.brianreber.library;
+
+import java.net.HttpURLConnection;
+import java.util.Arrays;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.brianreber.library.AuthUtil;
+
+public class AuthenticatedHttpRequest extends AsyncTask<String, Integer, String> {
+
+	/**
+	 * The context to get preferences with
+	 */
+	private Context mContext;
+
+	/**
+	 * The URL to request
+	 */
+	protected String url = "";
+	
+	/**
+	 * Whether this is a post request
+	 */
+	protected boolean isPost = false;
+	
+	public AuthenticatedHttpRequest(Context ctx) {
+		this.mContext = ctx;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+	 */
+	@Override
+	protected void onPostExecute(String r) {
+		Log.e(AuthenticatedHttpRequest.class.getName(), "Result: " + r);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.os.AsyncTask#doInBackground(Params[])
+	 */
+	@Override
+	protected String doInBackground(String... params) {
+		HttpClient client = new DefaultHttpClient();
+		HttpUriRequest request;
+		
+		if (isPost) {
+			request = new HttpPost(url);
+		} else {
+			request = new HttpGet(url);
+		}
+		
+		// Don't handle redirects - redirects get us into trouble when not logged in
+		client.getParams().setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, false);
+		request.setHeader("Content-Type", "application/json;charset=UTF-8");
+		request.setHeader("Cookie", AuthUtil.getAuthCookie(mContext));
+
+		try {
+			HttpResponse response = client.execute(request);
+
+			if (HttpURLConnection.HTTP_OK == response.getStatusLine().getStatusCode()) {
+				String contents = HttpUtils.readStreamAsString(response.getEntity().getContent());
+				return contents;
+			} else if (302 == response.getStatusLine().getStatusCode()) {
+				// We are redirecting - instead, invalidate the auth token and retry the request
+				// TODO:
+			} else {
+				Log.d("ART", "Status Code: " + response.getStatusLine().getStatusCode());
+				// TODO:
+			}
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+}
---
+++ src/com/brianreber/library/HttpUtils.java
@@ -0,0 +1,39 @@
+package com.brianreber.library;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author breber
+ */
+public class HttpUtils {
+
+	/**
+	 * Reads an entire input stream as a String. Closes the input stream.
+	 */
+	public static String readStreamAsString(InputStream in) {
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
+			byte[] buffer = new byte[1024];
+			int count;
+			do {
+				count = in.read(buffer);
+				if (count > 0) {
+					out.write(buffer, 0, count);
+				}
+			} while (count >= 0);
+			return out.toString("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("The JVM does not support the compiler's default encoding.", e);
+		} catch (IOException e) {
+			return null;
+		} finally {
+			try {
+				in.close();
+			} catch (IOException ignored) {	}
+		}
+	}
+
+}
